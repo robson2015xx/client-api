e4dc0f0c31ca924dccd242783577689c8df0a716
Robson Teixeira <robson2015xx@gmail.com>
2023-07-23 16:57:22 -0300

    initial commit

---
+++ src/test/java/br/robs/learn/clientapi/ClientApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.robs.learn.clientapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
